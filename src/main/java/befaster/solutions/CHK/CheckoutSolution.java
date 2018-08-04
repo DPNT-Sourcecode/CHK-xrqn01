ae51d6ef37428659855036aa163fb583b4711f30
sbx_user1060 <sbx_user1060@ip-10-11-38-153.eu-west-2.compute.internal>
2018-08-04 14:26:26 +0000

    Sat Aug 04 14:26:26 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -8,53 +8,44 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toMap;
 
 public class CheckoutSolution {
 
     private final static int INVALID_PRICE_VALUE = -1;
+    private final static Map<Character, Product> defaultProducts =
+            ImmutableMap.<Character, Product>builder().put('A', new Product('A', 50))
+                                                      .put('B', new Product('B', 50))
+                                                      .put('C', new Product('C', 20))
+                                                      .put('D', new Product('D', 15))
+                                                      .build();
+    private final static Map<Character, SpecialOffer> defaultSpecialOffers =
+            ImmutableMap.<Character, SpecialOffer>builder().put('A', new SpecialOffer(defaultProducts.get('A'), 3, 130))
+                                                           .put('B', new SpecialOffer(defaultProducts.get('B'), 2, 45))
+                                                           .build();
 
     private final Map<Character, Product> products;
     private final Map<Character, SpecialOffer> specialOffers;
 
     public CheckoutSolution() {
-        this.products = ImmutableMap.<Character, Product>builder().put('A', new Product('A', 50))
-                                                                  .put('B', new Product('B', 50))
-                                                                  .put('C', new Product('C', 20))
-                                                                  .put('D', new Product('D', 15))
-                                                                  .build();
-        this.specialOffers = ImmutableMap.<Character, SpecialOffer>builder().put('A', new SpecialOffer(products.get('A'), 3, 130))
-                                                                            .put('B', new SpecialOffer(products.get('B'), 2, 45))
-                                                                            .build();
+        this(defaultProducts, defaultSpecialOffers);
     }
 
-    public CheckoutSolution(List<Product> products, List<SpecialOffer> specialOffers) {
-        this.products = collectProductsListToMapById(products);
-        this.specialOffers = collectSpecialOffersToMapByProductId(specialOffers);
-    }
-
-    private Map<Character, Product> collectProductsListToMapById(List<Product> products) {
-        return products.stream()
-                       .collect(toMap(Product::getId, identity()));
-    }
-
-    private Map<Character, SpecialOffer> collectSpecialOffersToMapByProductId(List<SpecialOffer> specialOffers) {
-        return specialOffers.stream()
-                            .collect(toMap(SpecialOffer::getProductId, identity()));
+    public CheckoutSolution(Map<Character, Product> products, Map<Character, SpecialOffer> specialOffers) {
+        this.products = products;
+        this.specialOffers = specialOffers;
     }
 
     public Integer checkout(String skus) {
         return Optional.ofNullable(skus)
-                       .map(s -> skusStringToListOfProducts(s))
+                       .map(this::skusStringToListOfProducts)
                        .filter(this::areProductsValid)
                        .map(this::calculatePriceOfSortedProducts)
                        .orElse(INVALID_PRICE_VALUE);
     }
 
-    private List<Product> skusStringToListOfProducts(String s) {
-        return s.chars()
+    private List<Product> skusStringToListOfProducts(String skus) {
+        return skus.chars()
                    .mapToObj(id -> products.get((char) id))
                    .collect(toList());
     }
