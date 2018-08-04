29f49af5feee4d7b53425f597b74d0212ee9c6bb
sbx_user1060 <sbx_user1060@ip-10-11-38-153.eu-west-2.compute.internal>
2018-08-04 13:32:26 +0000

    Sat Aug 04 13:32:26 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,9 +2,23 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CheckoutSolution {
 
+    private final List<Product> products;
+    private final List<SpecialOffer> specialOffers;
 
+    public CheckoutSolution() {
+        this(Arrays.asList(new Product('A', 50)),
+             Arrays.asList(new SpecialOffer()));
+    }
+
+    public CheckoutSolution(List<Product> products, List<SpecialOffer> specialOffers) {
+        this.products = products;
+        this.specialOffers = specialOffers;
+    }
 
     public Integer checkout(String skus) {
         throw new SolutionNotImplementedException();
--- src/main/java/befaster/solutions/CHK/SpecialOffer.java
+++ src/main/java/befaster/solutions/CHK/SpecialOffer.java
@@ -2,18 +2,18 @@ package befaster.solutions.CHK;
 
 public final class SpecialOffer {
 
-    private final char productId;
+    private final Product product;
     private final int productsAmount;
     private final int discountPrice;
 
     public SpecialOffer(Product product, int productsAmount, int discountPrice) {
-        this.productId = product.getId();
+        this.product = product;
         this.productsAmount = productsAmount;
         this.discountPrice = discountPrice;
     }
 
     public char getProductId() {
-        return productId;
+        return product.getId();
     }
 
     public int getProductsAmount() {
