bf01debf67f417d96c7a1e68490d6d60f67511db
sbx_user1060 <sbx_user1060@ip-10-11-38-153.eu-west-2.compute.internal>
2018-08-04 13:47:26 +0000

    Sat Aug 04 13:47:26 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,20 +2,24 @@ package befaster.solutions.CHK;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
 
 public class CheckoutSolution {
 
-    private final List<Product> products;
+    private final Map<Character, Product> products;
     private final List<SpecialOffer> specialOffers;
 
     public CheckoutSolution() {
         final Product productA = new Product('A', 50);
         final Product productB = new Product('B', 30);
-        this.products = Arrays.asList(productA,
-                                      productB,
-                                      new Product('C', 20),
-                                      new Product('D', 15));
+        this.products = collectProductsListToMapById(Arrays.asList(productA,
+                                                                   productB,
+                                                                   new Product('C', 20),
+                                                                   new Product('D', 15)));
         this.specialOffers = Arrays.asList(new SpecialOffer(productA, 3, 130),
                                            new SpecialOffer(productB, 2, 45));
     }
@@ -25,8 +29,16 @@ public class CheckoutSolution {
         this.specialOffers = specialOffers;
     }
 
+    private final Map<Character, Product> collectProductsListToMapById(List<Product> products) {
+        return products.stream()
+                       .collect(toMap(Product::getId, identity()));
+    }
+
     public Integer checkout(String skus) {
         return Optional.ofNullable(skus)
+                       .map(s -> s.chars()
+                                  .sorted()
+                                  .mapToObj())
                        .orElse(-1);
     }
 }
