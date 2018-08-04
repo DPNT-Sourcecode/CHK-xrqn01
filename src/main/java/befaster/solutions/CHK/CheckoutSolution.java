ed9c0f266db5c9d02f2b17f10d10cdd845bb4088
sbx_user1060 <sbx_user1060@ip-10-11-38-153.eu-west-2.compute.internal>
2018-08-04 14:08:26 +0000

    Sat Aug 04 14:08:26 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -3,11 +3,11 @@ package befaster.solutions.CHK;
 import com.google.common.collect.HashMultiset;
 import com.google.common.collect.Multiset;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import static java.util.Arrays.asList;
 import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toMap;
@@ -17,27 +17,32 @@ public class CheckoutSolution {
     private final static int INVALID_PRICE_VALUE = -1;
 
     private final Map<Character, Product> products;
-    private final Map<Product, SpecialOffer> specialOffers;
+    private final Map<Character, SpecialOffer> specialOffers;
 
     public CheckoutSolution() {
         final Product productA = new Product('A', 50);
         final Product productB = new Product('B', 30);
-        this.products = collectProductsListToMapById(Arrays.asList(productA,
-                                                                   productB,
-                                                                   new Product('C', 20),
-                                                                   new Product('D', 15)));
-        this.specialOffers = Arrays.asList(productA, new SpecialOffer(productA, 3, 130),
-                                           productB, new SpecialOffer(productB, 2, 45));
+        this.products = collectProductsListToMapById(asList(productA,
+                                                            productB,
+                                                            new Product('C', 20),
+                                                            new Product('D', 15)));
+        this.specialOffers = collectSpecialOffersToMapByProductId(asList(new SpecialOffer(productA, 3, 130),
+                                                                         new SpecialOffer(productB, 2, 45)));
     }
 
     public CheckoutSolution(List<Product> products, List<SpecialOffer> specialOffers) {
         this.products = collectProductsListToMapById(products);
-        this.specialOffers = specialOffers;
+        this.specialOffers = collectSpecialOffersToMapByProductId(specialOffers);
     }
 
-    private final Map<Character, Product> collectProductsListToMapById(List<Product> products) {
+    private Map<Character, Product> collectProductsListToMapById(List<Product> products) {
         return products.stream()
                        .collect(toMap(Product::getId, identity()));
+    }
+
+    private Map<Character, SpecialOffer> collectSpecialOffersToMapByProductId(List<SpecialOffer> specialOffers) {
+        return specialOffers.stream()
+                            .collect(toMap(SpecialOffer::getProductId, identity()));
     }
 
     public Integer checkout(String skus) {
@@ -60,6 +65,9 @@ public class CheckoutSolution {
     }
 
     private int toProductValue(Multiset.Entry<Product> productEntry) {
+        final int productOccurrencesCount = productEntry.getCount();
+        final Product product = productEntry.getElement();
+        final SpecialOffer specialOffer = specialOffers.get(product.getId());
 
     }
 }
