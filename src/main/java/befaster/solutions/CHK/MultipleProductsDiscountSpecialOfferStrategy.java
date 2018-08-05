65596076153efd8a91eed9d057434e173cbd574c
sbx_user1060 <sbx_user1060@ip-10-12-74-13.eu-west-2.compute.internal>
2018-08-05 11:44:20 +0000

    Sun Aug 05 11:44:20 UTC 2018

--- src/main/java/befaster/solutions/CHK/MultipleProductsDiscountSpecialOfferStrategy.java
+++ src/main/java/befaster/solutions/CHK/MultipleProductsDiscountSpecialOfferStrategy.java
@@ -1,6 +1,7 @@
 package befaster.solutions.CHK;
 
 import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 
@@ -18,20 +19,22 @@ class MultipleProductsDiscountSpecialOfferStrategy implements SpecialOfferStrate
     public OfferApplicationResult applySpecialOfferTo(HashMultiset<Product> products) {
         products.entrySet()
                 .stream()
-                .filter(e -> productId == e.getElement()
-                                           .getId())
+                .filter(productEntry -> productEntry.getElement()
+                                                    .getId() == productId)
                 .findAny()
-                .map(productEntry -> {
-                    final int applicableProductsAmount = productEntry.getCount();
-                    final int applyOfferTimes = applicableProductsAmount / productsAmount;
-                    final int applyOfferToProducts = applyOfferTimes * productsAmount;
-                    final int discountPriceTotal = discountPrice * applyOfferTimes;
-                    final int nonDiscountedProductsAmount = applicableProductsAmount - applyOfferToProducts;
+                .map(productEntry -> applySpecialOfferTo(products, productEntry));
+    }
 
-                    final HashMultiset<Product> remainingProducts = HashMultiset.create(products);
-                    remainingProducts.setCount(productEntry.getElement(), nonDiscountedProductsAmount);
-                    return new OfferApplicationResult(products, discountPriceTotal);
-                });
+    private OfferApplicationResult applySpecialOfferTo(HashMultiset<Product> products, Multiset.Entry<Product> productEntry) {
+        final int applicableProductsAmount = productEntry.getCount();
+        final int applyOfferTimes = applicableProductsAmount / productsAmount;
+        final int applyOfferToProducts = applyOfferTimes * productsAmount;
+        final int discountPriceTotal = discountPrice * applyOfferTimes;
+        final int nonDiscountedProductsAmount = applicableProductsAmount - applyOfferToProducts;
+
+        final HashMultiset<Product> remainingProducts = HashMultiset.create(products);
+        remainingProducts.setCount(productEntry.getElement(), nonDiscountedProductsAmount);
+        return new OfferApplicationResult(remainingProducts, discountPriceTotal);
     }
 
 }
