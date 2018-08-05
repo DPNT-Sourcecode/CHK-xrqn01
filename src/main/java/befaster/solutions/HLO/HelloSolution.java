f77d3322418a56e89405703c47956f27e094affd
sbx_user1061 <sbx_user1061@ip-10-11-208-86.eu-west-2.compute.internal>
2018-08-05 11:06:34 +0000

    Sun Aug 05 11:06:34 UTC 2018

--- src/main/java/befaster/solutions/HLO/HelloSolution.java
+++ src/main/java/befaster/solutions/HLO/HelloSolution.java
@@ -1,6 +1,23 @@
 package befaster.solutions.HLO;
 
+import java.awt.*;
+import java.util.Optional;
+
 public class HelloSolution {
+
+    // PLEASE DON'T MIND ME, I'M JUST TESTING THE RECORDER
+
+    public static void main(String[] args) {
+        final Dimension screenSize = Toolkit.getDefaultToolkit()
+                                            .getScreenSize();
+        Optional.ofNullable(GraphicsEnvironment.getLocalGraphicsEnvironment())
+                .map(GraphicsEnvironment::getDefaultScreenDevice)
+                .map(GraphicsDevice::getDefaultConfiguration)
+                .map(GraphicsConfiguration::getBounds)
+                .ifPresent(System.out::println);
+        System.out.println(new Rectangle(screenSize));
+    }
+
     public String hello(String friendName) {
         return "Hello, " + friendName + '!';
     }
