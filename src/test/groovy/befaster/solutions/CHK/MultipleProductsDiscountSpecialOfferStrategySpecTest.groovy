8c854c7a99b383f04cde6cccf72571de067264e1
sbx_user1060 <sbx_user1060@ip-10-12-74-13.eu-west-2.compute.internal>
2018-08-05 11:20:20 +0000

    Sun Aug 05 11:20:20 UTC 2018

--- src/test/groovy/befaster/solutions/CHK/MultipleProductsDiscountSpecialOfferStrategySpecTest.groovy
+++ src/test/groovy/befaster/solutions/CHK/MultipleProductsDiscountSpecialOfferStrategySpecTest.groovy
@@ -2,17 +2,26 @@ package befaster.solutions.CHK
 
 import spock.lang.Specification
 import spock.lang.Subject
+import spock.lang.Unroll
 
 class MultipleProductsDiscountSpecialOfferStrategySpecTest extends Specification {
 
     static TEST_VALID_PRODUCT_ID = 'T' as char
     static TEST_INVALID_PRODUCT_ID = 'X' as char
-    static REQIRED_PRODUCTS_AMOUNT = 2
-    static PRODUCT_PRICE_WITHOUT_DISCOUNT = 10
+    static REQIRED_PRODUCTS_AMOUNT = 2 as int
+    static PRODUCT_PRICE_WITHOUT_DISCOUNT = 10 as int
+    static PRODUCTS_PRICE_WITH_DISCOUNT = (PRODUCTS_PRICE_WITH_DISCOUNT / 2) as int
+    static TEST_INVALID_PRODUCT = new Product(TEST_VALID_PRODUCT_ID, PRODUCT_PRICE_WITHOUT_DISCOUNT)
     static TEST_PRODUCT = new Product(TEST_VALID_PRODUCT_ID, PRODUCT_PRICE_WITHOUT_DISCOUNT)
 
     @Subject
-    def strategy = new MultipleProductsDiscountSpecialOfferStrategy(TEST_PRODUCT,)
+    def strategy = new MultipleProductsDiscountSpecialOfferStrategy(TEST_PRODUCT, REQIRED_PRODUCTS_AMOUNT, PRODUCTS_PRICE_WITH_DISCOUNT)
+
+    @Unroll
+    "should return remaining products=#expectedRemainingProducts and price=#expectedPrice for given #givenProducts"() {
+        when:
+          strategy.
+    }
 
 }
 
