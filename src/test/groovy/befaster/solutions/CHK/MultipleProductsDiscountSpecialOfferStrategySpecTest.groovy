6c7bea5975d01053fef143900a9d2e13d6d56059
sbx_user1060 <sbx_user1060@ip-10-12-74-13.eu-west-2.compute.internal>
2018-08-05 11:23:20 +0000

    Sun Aug 05 11:23:20 UTC 2018

--- src/test/groovy/befaster/solutions/CHK/MultipleProductsDiscountSpecialOfferStrategySpecTest.groovy
+++ src/test/groovy/befaster/solutions/CHK/MultipleProductsDiscountSpecialOfferStrategySpecTest.groovy
@@ -1,26 +1,39 @@
 package befaster.solutions.CHK
 
+import com.google.common.collect.HashMultiset
 import spock.lang.Specification
 import spock.lang.Subject
 import spock.lang.Unroll
 
 class MultipleProductsDiscountSpecialOfferStrategySpecTest extends Specification {
 
-    static TEST_VALID_PRODUCT_ID = 'T' as char
-    static TEST_INVALID_PRODUCT_ID = 'X' as char
-    static REQIRED_PRODUCTS_AMOUNT = 2 as int
+    static PRODUCT_ID = 'T' as char
+    static INVALID_PRODUCT_ID = 'X' as char
+    static REQUIRED_PRODUCTS_AMOUNT = 2 as int
     static PRODUCT_PRICE_WITHOUT_DISCOUNT = 10 as int
     static PRODUCTS_PRICE_WITH_DISCOUNT = (PRODUCTS_PRICE_WITH_DISCOUNT / 2) as int
-    static TEST_INVALID_PRODUCT = new Product(TEST_VALID_PRODUCT_ID, PRODUCT_PRICE_WITHOUT_DISCOUNT)
-    static TEST_PRODUCT = new Product(TEST_VALID_PRODUCT_ID, PRODUCT_PRICE_WITHOUT_DISCOUNT)
+    static INVALID_PRODUCT = new Product(INVALID_PRODUCT_ID, 1000)
+    static PRODUCT = new Product(PRODUCT_ID, PRODUCT_PRICE_WITHOUT_DISCOUNT)
 
     @Subject
-    def strategy = new MultipleProductsDiscountSpecialOfferStrategy(TEST_PRODUCT, REQIRED_PRODUCTS_AMOUNT, PRODUCTS_PRICE_WITH_DISCOUNT)
+    def strategy = MultipleProductsDiscountSpecialOfferStrategy.builder().productId(PRODUCT.getId())
+                                                               .productsAmount(REQUIRED_PRODUCTS_AMOUNT)
+                                                               .discountPrice(PRODUCTS_PRICE_WITH_DISCOUNT)
+                                                               .build()
 
     @Unroll
     "should return remaining products=#expectedRemainingProducts and price=#expectedPrice for given #givenProducts"() {
+        given:
+          HashMultiset.create()
+
         when:
-          strategy.
+          strategy.applySpecialOfferTo()
+
+        where:
+          givenProducts              || expectedRemainingProducts  | expectedPrice
+          []                         || []                         | 0
+          [PRODUCT, INVALID_PRODUCT] || [PRODUCT, INVALID_PRODUCT] | 0
+          [PRODUCT, PRODUCT]         || []                         | PRODUCTS_PRICE_WITH_DISCOUNT
     }
 
 }
