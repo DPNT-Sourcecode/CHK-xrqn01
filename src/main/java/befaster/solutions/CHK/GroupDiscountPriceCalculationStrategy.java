44b792ac8f57a499e17a3d2c6ca685f774cca33a
sbx_user1060 <sbx_user1060@ip-10-12-166-102.eu-west-2.compute.internal>
2018-08-06 09:54:15 +0000

    Mon Aug 06 09:54:15 UTC 2018

---
+++ src/main/java/befaster/solutions/CHK/GroupDiscountPriceCalculationStrategy.java
@@ -0,0 +1,38 @@
+package befaster.solutions.CHK;
+
+import com.google.common.collect.HashMultiset;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Builder
+@RequiredArgsConstructor(access = PRIVATE)
+final class GroupDiscountPriceCalculationStrategy implements PriceCalculationStrategy {
+
+    private final List<Character> applicableProducts;
+    private final int requiredProductsAmount;
+    private final int discountedPrice;
+
+    @Override
+    public boolean isApplicableTo(Product product) {
+        return applicableProducts.contains(product.getId());
+    }
+
+    @Override
+    public PriceCalculationResult applySpecialOfferTo(HashMultiset<Product> products) {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        String applicableProductsString = applicableProducts.stream()
+                                                            .map(Object::toString)
+                                                            .collect(Collectors.joining(","));
+        return "buy any " + requiredProductsAmount + " of (S,T,X,Y,Z) for 45";
+    }
+
+}
