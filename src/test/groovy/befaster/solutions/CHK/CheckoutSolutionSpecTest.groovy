992929f7bfe015c878a4a8883b79784161fe384e
sbx_user1060 <sbx_user1060@ip-10-12-166-102.eu-west-2.compute.internal>
2018-08-06 09:42:15 +0000

    Mon Aug 06 09:42:15 UTC 2018

--- src/test/groovy/befaster/solutions/CHK/CheckoutSolutionSpecTest.groovy
+++ src/test/groovy/befaster/solutions/CHK/CheckoutSolutionSpecTest.groovy
@@ -20,30 +20,151 @@ class CheckoutSolutionSpecTest extends Specification {
           actualPrice == expectedPrice
 
         where:
-          productsString          || expectedPrice
-          null                    || -1
-          "${INVALID_PRODUCT_ID}" || -1
-          ""                      || 0
-          "A"                     || 50
-          "AAA"                   || 130
-          "AAABB"                 || 130 + 45
-          "BBAAA"                 || 130 + 45
-          "BAAAB"                 || 130 + 45
-          "AAAAA"                 || 200
-          "AAAAAA"                || 250
-          "B"                     || 30
-          "BB"                    || 45
-          "EEB"                   || (2 * 40)
-          "EEBB"                  || (2 * 40) + 30
-          "AAAEEBB"               || 130 + (2 * 40) + 30
-          "FF"                    || 2 * 10
-          "FFF"                   || 2 * 10
-          "FFFF"                  || 3 * 10
-          "FFFFF"                 || 4 * 10
-          "FFFFFF"                || 4 * 10
-          "HHHHHHHHHH"            || 80
-          "HHHHHHHHHHH"            || 90
-          "HHHHHHHHHHHH"            || 100
+          productsString                                               || expectedPrice
+          null                                                         || -1
+          "${INVALID_PRODUCT_ID}"                                      || -1
+          ""                                                           || 0
+          "A"                                                          || 50
+          "B"                                                          || 30
+          "C"                                                          || 20
+          "D"                                                          || 15
+          "E"                                                          || 40
+          "F"                                                          || 10
+          "G"                                                          || 20
+          "H"                                                          || 10
+          "I"                                                          || 35
+          "J"                                                          || 60
+          "K"                                                          || 80
+          "L"                                                          || 90
+          "M"                                                          || 15
+          "N"                                                          || 40
+          "O"                                                          || 10
+          "P"                                                          || 50
+          "Q"                                                          || 30
+          "R"                                                          || 50
+          "S"                                                          || 30
+          "T"                                                          || 20
+          "U"                                                          || 40
+          "V"                                                          || 50
+          "W"                                                          || 20
+          "X"                                                          || 90
+          "Y"                                                          || 10
+          "Z"                                                          || 50
+          "a"                                                          || -1
+          "-"                                                          || -1
+          "ABCa"                                                       || -1
+          "AxA"                                                        || -1
+          "ABCDEFGHIJKLMNOPQRSTUVWXYZ"                                 || 965
+          "A"                                                          || 50
+          "AA"                                                         || 100
+          "AAA"                                                        || 130
+          "AAAA"                                                       || 180
+          "AAAAA"                                                      || 200
+          "AAAAAA"                                                     || 250
+          "AAAAAAA"                                                    || 300
+          "AAAAAAAA"                                                   || 330
+          "AAAAAAAAA"                                                  || 380
+          "AAAAAAAAAA"                                                 || 400
+          "P"                                                          || 50
+          "PP"                                                         || 100
+          "PPP"                                                        || 150
+          "PPPP"                                                       || 200
+          "PPPPP"                                                      || 200
+          "PPPPPP"                                                     || 250
+          "PPPPPPP"                                                    || 300
+          "PPPPPPPP"                                                   || 350
+          "PPPPPPPPP"                                                  || 400
+          "PPPPPPPPPP"                                                 || 400
+          "UUU"                                                        || 120
+          "UUUU"                                                       || 120
+          "UUUUU"                                                      || 160
+          "UUUUUUUU"                                                   || 240
+          "UUUUUUUU"                                                   || 240
+          "EE"                                                         || 80
+          "EEB"                                                        || 80
+          "EEEB"                                                       || 120
+          "EEEEBB"                                                     || 160
+          "BEBEEE"                                                     || 160
+          "RRR"                                                        || 150
+          "RRRQ"                                                       || 150
+          "RRRRQ"                                                      || 200
+          "RRRRRRQQ"                                                   || 300
+          "RRRQRQRR"                                                   || 300
+          "A"                                                          || 50
+          "AA"                                                         || 100
+          "AAA"                                                        || 130
+          "AAAA"                                                       || 180
+          "AAAAA"                                                      || 200
+          "AAAAAA"                                                     || 250
+          "H"                                                          || 10
+          "HH"                                                         || 20
+          "HHH"                                                        || 30
+          "HHHH"                                                       || 40
+          "HHHHH"                                                      || 45
+          "HHHHHH"                                                     || 55
+          "HHHHHHH"                                                    || 65
+          "HHHHHHHH"                                                   || 75
+          "HHHHHHHHH"                                                  || 85
+          "HHHHHHHHHH"                                                 || 80
+          "HHHHHHHHHHH"                                                || 90
+          "HHHHHHHHHHHH"                                               || 100
+          "HHHHHHHHHHHHH"                                              || 110
+          "HHHHHHHHHHHHHH"                                             || 120
+          "HHHHHHHHHHHHHHH"                                            || 125
+          "HHHHHHHHHHHHHHHH"                                           || 135
+          "HHHHHHHHHHHHHHHHH"                                          || 145
+          "HHHHHHHHHHHHHHHHHH"                                         || 155
+          "HHHHHHHHHHHHHHHHHHH"                                        || 165
+          "HHHHHHHHHHHHHHHHHHHH"                                       || 160
+          "V"                                                          || 50
+          "VV"                                                         || 90
+          "VVV"                                                        || 130
+          "VVVV"                                                       || 180
+          "VVVVV"                                                      || 220
+          "VVVVVV"                                                     || 260
+          "B"                                                          || 30
+          "BB"                                                         || 45
+          "BBB"                                                        || 75
+          "BBBB"                                                       || 90
+          "NNN"                                                        || 120
+          "NNNM"                                                       || 120
+          "NNNNM"                                                      || 160
+          "NNNNNNMM"                                                   || 240
+          "NNNMNMNN"                                                   || 240
+          "FF"                                                         || 20
+          "FFF"                                                        || 20
+          "FFFF"                                                       || 30
+          "FFFFFF"                                                     || 40
+          "FFFFFF"                                                     || 40
+          "K"                                                          || 80
+          "KK"                                                         || 150
+          "KKK"                                                        || 230
+          "KKKK"                                                       || 300
+          "Q"                                                          || 30
+          "QQ"                                                         || 60
+          "QQQ"                                                        || 80
+          "QQQQ"                                                       || 110
+          "QQQQQ"                                                      || 140
+          "QQQQQQ"                                                     || 160
+          "V"                                                          || 50
+          "VV"                                                         || 90
+          "VVV"                                                        || 130
+          "VVVV"                                                       || 180
+          "H"                                                          || 10
+          "HH"                                                         || 20
+          "HHH"                                                        || 30
+          "HHHH"                                                       || 40
+          "HHHHH"                                                      || 45
+          "HHHHHH"                                                     || 55
+          "HHHHHHH"                                                    || 65
+          "HHHHHHHH"                                                   || 75
+          "HHHHHHHHH"                                                  || 85
+          "HHHHHHHHHH"                                                 || 80
+          "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"       || 1880
+          "LGCKAQXFOSKZGIWHNRNDITVBUUEOZXPYAVFDEPTBMQLYJRSMJCWH"       || 1880
+          "AAAAAPPPPPUUUUEEBRRRQAAAHHHHHHHHHHVVVBBNNNMFFFKKQQQVVHHHHH" || 1640
+          "PPPPQRUVPQRUVPQRUVSU"                                       || 740
     }
+
 
 }
